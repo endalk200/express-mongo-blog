93137ee2c963b191bc76eda6b33df9e7ff8801d0
endalk2 <eb808826@gmail.com>
2021-01-11 16:51:55 +0300

    added static assets

---
+++ static/index.js
@@ -0,0 +1,7 @@
+/**
+ * Import your primary style here. The primary style is in sass format.
+ * by importing it here it will be compiled and minified to css format
+ */
+style = require("../static/sass/style.scss")
+
+require("bootstrap")
